a1b8f877dfc16ac28fc8bbf6940488ca7d53a61b
Andrea Lopez Palomeque <andrealopezpalomeque@gmail.com>
2024-01-22 14:20:43 -0300

    Initial Commit

---
+++ GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Andrea Victoria López Palomeque on 22/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
