ccbece990c43145c37e5ac8632566176e8102b82
Andrea Lopez Palomeque <andrealopezpalomeque@gmail.com>
2024-01-22 14:43:53 -0300

    vstack buttons

--- GuessTheFlag/ContentView.swift
+++ GuessTheFlag/ContentView.swift
@@ -9,7 +9,17 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Button("Delete selection", role: .destructive, action: executeDelete)
+        VStack {
+            Button("Button 1") { }
+                .buttonStyle(.bordered)
+            Button("Button 2", role: .destructive) { }
+                .buttonStyle(.bordered)
+            Button("Button 3") { }
+                .buttonStyle(.borderedProminent)
+                .tint(.indigo)
+            Button("Button 4", role: .destructive) { }
+                .buttonStyle(.borderedProminent)
+        }
     }
 
     func executeDelete() {
